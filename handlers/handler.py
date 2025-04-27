09a411107bef20473408d0ceb5d7c99246091cf7
SOMEL <robomax2009@gmail.com>
2025-04-27 18:31:16 +0500

    8

--- handlers/handler.py
+++ handlers/handler.py
@@ -2,6 +2,7 @@ from TgCloud.TgCloud import *
 from TgCloud import config
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def account(request):
