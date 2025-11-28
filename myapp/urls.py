3f3723cfdf850b77b2159bee5fd1f858280bce28
Somel <gagusus228@gmail.com>
2025-11-28 07:28:50 +0500

    46

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,11 +6,8 @@ from handlers import seccion
 import threading
 from constant_tasks import task_everyday
 
-threading.Thread(target=task_everyday.delay, daemon=True).start()
-
-
 urlpatterns = [
     path('admin_check/', handler.admin_check),
     path('check_code/', handler.check_code),
-    path('update_news943411234540/', handler.update_news),
+    path('update_news/', handler.update_news),
 ]
