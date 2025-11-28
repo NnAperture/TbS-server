bc744512eca68d53e73482172f7986e717b60efc
Somel <gagusus228@gmail.com>
2025-11-28 07:31:22 +0500

    47

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,7 +4,6 @@ from handlers import handler
 from handlers import seccion
 
 import threading
-from constant_tasks import task_everyday
 
 urlpatterns = [
     path('admin_check/', handler.admin_check),
